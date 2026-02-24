ffb768043bf6b8d3cdd6edf075c1d67d76c67349
Sumayyasumi123 <sumayyaans3113@gmail.com>
2026-02-24 20:51:14 +0530

    welcome card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import internalImage from "./images/welcome.jpg";
 
 function App() {
+  const userName = "sumayya"; 
+
+  console.log("React app started");
+
+  const headingStyle = {
+    color: "#0d6efd",
+    fontWeight: "bold",
+    textAlign: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card shadow p-4 text-center" style={{ width: "24rem" }}>
+        <h2 style={headingStyle}>
+          Welcome to React Learning, {userName}
+        </h2>
+
+        {/* Internal Image */}
+        <img
+          src={internalImage}
+          alt="Internal"
+          className="img-fluid mx-auto my-3"
+          style={{ width: "200px" }}
+        />
+
+        {/* External Image */}
+        <img
+          src="https://i.pinimg.com/1200x/06/7c/10/067c105976b1322bba870ad3e6652a95.jpg"
+          alt="External"
+          className="img-fluid mx-auto my-3"
+        />
+
+        <p className="text-muted fw-semibold">
+          This is your first card with images and styles!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
